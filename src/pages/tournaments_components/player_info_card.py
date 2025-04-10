97f5ba8f0b2a4af4d5b91fbbd21906cc3543d61f
gamerbeaker <gamerbeaker@gmail.com>
2025-04-10 20:25:40 +0200

    update style

--- src/pages/tournaments_components/player_info_card.py
+++ src/pages/tournaments_components/player_info_card.py
@@ -1,6 +1,5 @@
 from src.utils.icons import player_icon_url
 
-# Define CSS styles
 player_info_styles = """
 <style>
 .player-card {
@@ -12,25 +11,37 @@ player_info_styles = """
     justify-content: flex-start;
     background-color: transparent;
     padding-left: 10px;
+    border-radius: 10px;
+    overflow: hidden;
+}
+
+.player-card-link {
+    display: flex;
+    align-items: center;
+    text-decoration: none !important;
+    color: inherit;
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+    border-radius: 10px;
     transition: background 0.2s ease;
 }
-.player-card:hover {
+
+.player-card-link:hover {
     background-color: rgba(255, 255, 255, 0.05);
 }
+
 .player-card img {
     height: 40px;
     opacity: 0.8;
     margin-right: 12px;
 }
+
 .player-card span {
     color: white;
     font-weight: bold;
     font-size: 18px;
-}
-.player-card-link {
-    display: flex;
-    align-items: center;
-    text-decoration: none;
+    text-decoration: none !important;
 }
 </style>
 """
@@ -38,7 +49,7 @@ player_info_styles = """
 
 def get_player_info_card(row):
     return f"""{player_info_styles}<div class='player-card'>
-            <a href='inspect?player={row["player"]}' target='_self' class='player-card-link '>
+            <a href='inspect?player={row["player"]}' target='_self' class='player-card-link'>
                 <img src='{player_icon_url}' alt='player icon' />
                 <span>{row["player"]}</span>
             </a>
