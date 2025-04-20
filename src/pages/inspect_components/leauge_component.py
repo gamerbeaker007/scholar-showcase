c2a08497c315d2845be6bd2853ccd769d3ffbd15
gamerbeaker <gamerbeaker@gmail.com>
2025-04-20 12:40:11 +0200

    Add some extra information to the inspect player page. Add some logging.

--- src/pages/inspect_components/leauge_component.py
+++ src/pages/inspect_components/leauge_component.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 
 from src.utils.icons import WEB_URL
@@ -68,3 +69,24 @@ def league_info(league_df, format_type):
         """,
         unsafe_allow_html=True
     )
+
+
+def add_league_cards(player_info_dict):
+    modern_df = pd.DataFrame(player_info_dict['season_details']['modern'], index=[0])
+    wild_df = pd.DataFrame(player_info_dict['season_details']['wild'], index=[0])
+    survival_df = pd.DataFrame(player_info_dict['season_details']['survival'], index=[0])
+    league_data = []
+    if not modern_df.empty:
+        league_data.append(("modern", modern_df))
+    if not wild_df.empty:
+        league_data.append(("wild", wild_df))
+    if not survival_df.empty:
+        league_data.append(("survival", survival_df))
+
+    # Show cards in left-to-right order using columns
+    st.markdown(league_info_style, unsafe_allow_html=True)
+
+    columns = st.columns(3)
+    for i, (format_type, df) in enumerate(league_data):
+        with columns[i]:
+            league_info(df, format_type)
